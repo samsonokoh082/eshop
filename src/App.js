77e6cc087cac882904eb91a48343e95993898cce
samsonokoh082 <samsonokoh082@gmail.com>
2023-03-12 18:22:47 +0100

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 
 import './App.css';
+import Header from './header';
+
 
 function App() {
   return (
-    <div className='App'>
-      <div className="header_logo">
-
-      </div>
-
-      <div className="header_search">
-
-      </div>
-
-      <div className="header_nav">
-
-        <div className="nav_item">
-          <span className="nav_itemLineOne">Hello Guest</span>
-          <span className="nav_itemLineTwo">Sign In my name</span>
-
-
-        </div>
-        <div className="nav_item">
-
-        <span className="nav_itemLineOne">Your</span>
-          <span className="nav_itemLineTwo">Shop</span>
-
-        </div>
-        <div className="nav_item">
-
-        <span className="nav_itemLineOne">Basket</span>
-          <span className="nav_itemLineTwo">0</span>
-
-        </div>
-      </div>
-
-    </div>
+    <Header />
   );
 }
 
---
+++ src/header.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './header.css';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Header = () => {
+  return (
+    <div className='header'>
+  
+      <div className="header_logo">
+        <StorefrontIcon className='header__logoImage' fontSize='large'/>
+        <h2 className='header__logoTitle'> eShop</h2>
+
+      </div>
+
+      <div className="header_search">
+        <input type="text" className='header__searchInput' />
+        <SearchIcon className='header__searchIcon'/>
+
+      </div>
+
+      <div className="header_nav">
+
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Hello Guest</span>
+          <span className="nav_itemLineTwo">Sign In my name</span>
+
+
+        </div>
+        <div className="nav_item">
+
+        <span className="nav_itemLineOne">Your</span>
+          <span className="nav_itemLineTwo">Shop</span>
+
+        </div>
+        <div className="nav_item">
+        <ShoppingBasketIcon fontSize='large' className='nav_itemBasket'/>
+         
+          <span className="nav_itemLineTwo nav__basketCount">0</span>
+
+        </div>
+      </div>
+
+    </div> 
+   
+  )
+}
+
+export default Header
